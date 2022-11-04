e311d8ca4d0a00e4d6a56a860de1f6780b4df3c9
Emanuel Schiendorfer <mandi.schiendorfer@gmail.com>
2022-11-04 16:56:01 +0100

    A lot of work on checkout

--- views/js/tb_framework.js
+++ views/js/tb_framework.js
@@ -46,8 +46,6 @@ function initComponent(component_name, component) {
         component.init();
     }
 
-    console.log(tb_framework);
-
 }
 
 function renderComponentWithAjax(
@@ -150,8 +148,6 @@ function initAjaxComponent(content, relative_element = document.body, relative_p
     var component_helper = document.createElement('div');
     component_helper.innerHTML = template.innerHTML.trim(); // Never return a text node of whitespace as the result
 
-    // Todo: before we had component_helper.firstElementChild -> this made problems for modal_default, as there are two elements
-    // Todo:  Check If it works now correctly for all elements
     var component = component_helper;
 
     // Note: insertAdjacentHTML has the drawback, that the callback element is not usable
@@ -490,3 +486,185 @@ function updateProductPrice(id_product, use_tax = true, id_product_attribute = 0
     }
 }
 
+
+// Some copied stuff from tools.js
+/*
+ * This also uses global priceDisplayPrecision, so this should be right.
+ * Formatting should match Tools::displayPrice().
+ */
+function displayPrice(price, currencyFormat, currencySign, currencyBlank) {
+    var currency = 'EUR';
+
+    if (typeof window.currency_iso_code !== 'undefined' && window.currency_iso_code.length === 3) {
+        // Should be exposed in Back Office
+        currency = window.currency_iso_code;
+    } else if (typeof window.currency === 'object' && typeof window.currency.iso_code !== 'undefined' && window.currency.iso_code.length === 3) {
+        // Front Office
+        currency = window.currency.iso_code;
+    }
+
+    if (isNaN(price) || price == '') {
+        price = 0;
+    }
+
+    if (typeof window['currencyFormatters'] !== 'undefined' && window.currencyFormatters[currency]) {
+        var formatter = window.currencyFormatters[currency];
+        var val = executeFunctionByName(formatter, [price, currencyFormat, currencySign, currencyBlank, priceDisplayPrecision]);
+        if (typeof val === 'string' || val instanceof String) {
+            return val;
+        }
+    }
+
+    var blank = '';
+    price = ps_round(price, priceDisplayPrecision);
+    if (currencyBlank > 0) {
+        blank = ' ';
+    }
+
+    // currencyFormat is available in front office, only.
+    if (currencyFormat == 1) {
+        return currencySign + blank + formatNumber(price, priceDisplayPrecision, ',', '.');
+    }
+    if (currencyFormat == 2) {
+        return (formatNumber(price, priceDisplayPrecision, ' ', ',') + blank + currencySign);
+    }
+    if (currencyFormat == 3) {
+        return (currencySign + blank + formatNumber(price, priceDisplayPrecision, '.', ','));
+    }
+    if (currencyFormat == 4) {
+        return (formatNumber(price, priceDisplayPrecision, ',', '.') + blank + currencySign);
+    }
+    if (currencyFormat == 5) {
+        return (currencySign + blank + formatNumber(price, priceDisplayPrecision, '\'', '.'));
+    }
+    if (currencyFormat == 6) {
+        return (formatNumber(price, priceDisplayPrecision, '.', ',') + blank + currencySign);
+    }
+
+    return price.toFixed(priceDisplayPrecision);
+}
+
+function ps_round_helper(value, mode) {
+    // From PHP Math.c
+    if (value >= 0.0) {
+        tmp_value = Math.floor(value + 0.5);
+        if ((mode == 3 && value == (-0.5 + tmp_value)) ||
+            (mode == 4 && value == (0.5 + 2 * Math.floor(tmp_value / 2.0))) ||
+            (mode == 5 && value == (0.5 + 2 * Math.floor(tmp_value / 2.0) - 1.0))) {
+            tmp_value -= 1.0;
+        }
+    }
+    else {
+        tmp_value = Math.ceil(value - 0.5);
+        if ((mode == 3 && value == (0.5 + tmp_value)) ||
+            (mode == 4 && value == (-0.5 + 2 * Math.ceil(tmp_value / 2.0))) ||
+            (mode == 5 && value == (-0.5 + 2 * Math.ceil(tmp_value / 2.0) + 1.0))) {
+            tmp_value += 1.0;
+        }
+    }
+
+    return tmp_value;
+}
+
+function ps_log10(value) {
+    return Math.log(value) / Math.LN10;
+}
+
+function ps_round_half_up(value, precision) {
+    var mul = Math.pow(10, precision);
+    var val = value * mul;
+
+    var next_digit = Math.floor(val * 10) - 10 * Math.floor(val);
+    if (next_digit >= 5) {
+        val = Math.ceil(val);
+    } else {
+        val = Math.floor(val);
+    }
+
+    return val / mul;
+}
+
+function ps_round(value, places) {
+    if (typeof(roundMode) === 'undefined') {
+        roundMode = 2;
+    }
+    if (typeof(places) === 'undefined') {
+        places = 2;
+    }
+    value = parseFloat(value);
+    if (isNaN(value)) {
+        return 0;
+    }
+
+    var method = roundMode;
+
+    if (method === 0) {
+        return ceilf(value, places);
+    } else if (method === 1) {
+        return floorf(value, places);
+    } else if (method === 2) {
+        return ps_round_half_up(value, places);
+    } else if (method == 3 || method == 4 || method == 5) {
+        // From PHP Math.c
+        var precision_places = 14 - Math.floor(ps_log10(Math.abs(value)));
+        var f1 = Math.pow(10, Math.abs(places));
+
+        if (precision_places > places && precision_places - places < 15) {
+            var f2 = Math.pow(10, Math.abs(precision_places));
+            if (precision_places >= 0) {
+                tmp_value = value * f2;
+            } else {
+                tmp_value = value / f2;
+            }
+
+            tmp_value = ps_round_helper(tmp_value, roundMode);
+
+            /* now correctly move the decimal point */
+            f2 = Math.pow(10, Math.abs(places - precision_places));
+            /* because places < precision_places */
+            tmp_value /= f2;
+        }
+        else {
+            /* adjust the value */
+            if (places >= 0) {
+                tmp_value = value * f1;
+            } else {
+                tmp_value = value / f1;
+            }
+
+            if (Math.abs(tmp_value) >= 1e15) {
+                return value;
+            }
+        }
+
+        tmp_value = ps_round_helper(tmp_value, roundMode);
+        if (places > 0) {
+            tmp_value = tmp_value / f1;
+        } else {
+            tmp_value = tmp_value * f1;
+        }
+
+        return tmp_value;
+    }
+}
+
+// return a formatted number
+function formatNumber(value, numberOfDecimal, thousenSeparator, virgule) {
+    value = value.toFixed(numberOfDecimal);
+    var val_string = value + '';
+    var tmp = val_string.split('.');
+    var abs_val_string = (tmp.length === 2) ? tmp[0] : val_string;
+    var deci_string = ('0.' + (tmp.length === 2 ? tmp[1] : 0)).substr(2);
+    var nb = abs_val_string.length;
+
+    for (var i = 1; i < 4; i++) {
+        if (value >= Math.pow(10, (3 * i))) {
+            abs_val_string = abs_val_string.substring(0, nb - (3 * i)) + thousenSeparator + abs_val_string.substring(nb - (3 * i));
+        }
+    }
+
+    if (parseInt(numberOfDecimal) === 0) {
+        return abs_val_string;
+    }
+    return abs_val_string + virgule + (deci_string > 0 ? deci_string : '00');
+}
